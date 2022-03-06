81c75127ff8dbe02ed686d39dc581492cb041303
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-03-06 16:41:55 +0300

    Update randombg.js

--- assets/js/randombg.js
+++ assets/js/randombg.js
@@ -1,10 +1,10 @@
 let nocolors = 5;
 let colors = {
-    1:["#3B6EBA","#739AD3"],
-    2:["#6D68BD","#F9CD3"],
-    3:["#210070","#213970"],
-    4:["#A3D8E0","#C9BDBB"],
-    5:["rgba(0, 98, 185, 0.8)","rgba(0, 98, 185, 0.8)"]
+    0:["#3B6EBA","#739AD3"],
+    1:["#6D68BD","#F9CD3"],
+    2:["#210070","#213970"],
+    3:["#A3D8E0","#C9BDBB"],
+    4:["rgba(0, 98, 185, 0.8)","rgba(0, 98, 185, 0.8)"]
 }
 function random_colors(choices) {
   var index = Math.floor(Math.random() * nocolors);
